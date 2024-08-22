2402d170430986b5bd0e2c2d2b7ff74321b41f10
linyq <linyqemail@163.com>
2024-08-22 09:32:34 +0800

    从环境变量获取 redis client

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,7 @@
+import os
 from redis import Redis
 
 
 # 初始化 Redis 客户端
 def get_redis_client():
-    return Redis(host="127.0.0.1", port=6379, db=1, decode_responses=True)
+    return Redis(host=os.getenv("REDIS_HOST", "127.0.0.1"), port=6379, db=1, decode_responses=True)
